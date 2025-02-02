7a2044b5d94baa501589508ce70867053e515329
QuickScythe <QuickScythe@gmail.com>
2025-02-02 12:51:18 -0600

    Nullable fix

--- src/main/java/me/quickscythe/dragonforge/api/events/DragonForgePlayerDeathEvent.java
+++ src/main/java/me/quickscythe/dragonforge/api/events/DragonForgePlayerDeathEvent.java
@@ -2,7 +2,7 @@ package me.quickscythe.dragonforge.api.events;
 
 import me.quickscythe.dragonforge.api.entity.DragonForgePlayer;
 import me.quickscythe.dragonforge.api.events.listeners.Listener;
-import org.jetbrains.annotations.Nullable;
+import org.eclipse.jgit.annotations.Nullable;
 
 public record DragonForgePlayerDeathEvent(DragonForgePlayer player, @Nullable DragonForgePlayer killer, String message) implements DragonForgeEvent {
     @Override
